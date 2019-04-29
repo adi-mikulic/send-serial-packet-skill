31b49b31f640b3b9393240dc2240a126a9815e16
Kiahna Tucker <33497275+Kiahnat36@users.noreply.github.com>
2019-04-29 02:20:54 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
+import serial
 
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
@@ -24,17 +25,19 @@ class SendSerialPacketSkill(MycroftSkill):
         # PYTHON_NOTE: Arrays do not exist within python, instead a data
         # structure known as a "list" has similar functionality.
         #
-        self.payload = []               # Message payload
+        self.payload = ''               # Message payload
         self.payload_size = 10          # Size of the payload in bytes
-        self.comm_protocol = []         # User-selected communication protocol identifier
-        self.port = []                  # User or system selected port to transmit data
+        self.payload_loop_control = 0
+        self.comm_protocol = ''         # User-selected communication protocol identifier
+        self.port = '01'                  # User or system selected port to transmit data
 
-        self.sync_bytes = []            # Sync bytes (if necessary)
+        self.sync_bytes = ''            # Sync bytes (if necessary)
         self.sync_loop_control = 0
 
         self.str_to_int = 0              # Variable used to convert string input to integer values
 
         self.serial_packet = []         # Complete data packet to send
+        self.ser = serial.Serial()
 
     # The "handle_xxxx_intent" function is triggered by Mycroft when the
     # skill's intent is matched.  The intent is defined by the IntentBuilder()
@@ -51,26 +54,55 @@ class SendSerialPacketSkill(MycroftSkill):
     @intent_handler(IntentBuilder("ProtocolIntent").require("Protocols").require("Format"))
     def handle_select_protocol_intent(self, message):
         if message.data["Format"] == "spi":
-            self.comm_protocol = [0,2]
+            self.comm_protocol = '02'
         elif message.data["Format"] == "uart":
-            self.comm_protocol = [0,1]
+            self.comm_protocol = '01'
         elif message.data["Format"] == "i squared c":
-            self.comm_protocol = [0,3]
+            self.comm_protocol = '03'
         else:
-            self.comm_protocol = [0,0]
+            self.comm_protocol = '00'
         self.speak_dialog("selected.protocol", data={"comm_protocol": self.comm_protocol})
 
     @intent_handler(IntentBuilder("SyncBytesIntent").require("Sync").require("HexNum"))
     def handle_sync_byte_intent(self, message):
         if self.sync_loop_control < 4:
             self.str_to_int = int(message.data["HexNum"],16)
-            if self.str_to_int > 15:
+            if self.str_to_int > 9:
                 self.speak_dialog("nibble.prompt")
             else:
                 self.sync_loop_control += 1
-                self.sync_bytes.append(hex(self.str_to_int).lstrip("0x"))
+                self.sync_bytes = self.sync_bytes + hex(self.str_to_int).lstrip("0x")
             self.speak_dialog("extend.sync", data={"sync_loop_control": self.sync_loop_control})
         self.speak_dialog("complete.sync", data={"sync_bytes": self.sync_bytes})
+    
+    @intent_handler(IntentBuilder("PortSelectIntent").require("PortName"))
+    def handle_port_select_intent(self, message):
+        self.ser.baudrate = 19200
+        self.ser.port = 'COM1'
+        self.speak_dialog("port.config", data={"port_name": self.ser.name})
+
+    @intent_handler(IntentBuilder("PayloadConfigIntent").require("Payload").require("HexByte"))
+    def handle_payload_select_intent(self, message):
+        self.str_to_int = 0
+        if self.payload_loop_control < self.payload_size:
+            self.payload_loop_control += 1
+            self.str_to_int = int(message.data["HexByte"],16)
+            self.payload = self.payload + hex(self.str_to_int).lstrip("0x")
+            self.speak_dialog("extend.payload", data={"payload_loop_control": self.payload_loop_control})
+        self.speak_dialog("complete.payload", data={"payload": self.payload})
+    
+    @intent_handler(IntentBuilder("BuildSerialPacketIntent").require("BuildSerial"))
+    def handle_build_serial_packet_intent(self, message):
+        self.serial_packet.append(self.sync_bytes)
+        self.serial_packet.append(self.comm_protocol)
+        self.serial_packet.append(self.port)
+        self.serial_packet.append(self.payload)
+
+        self.serial_packet = bytearray(self.serial_packet,'utf-8')
+        self.ser.open()
+        self.ser.write(self.serial_packet)
+        self.speak_dialog("send.serial.data")
+        self.ser.close()
 
     # @intent_handler(IntentBuilder("").require("Data").require("Packet"))
     # def handle_serial_intent(self, message):
